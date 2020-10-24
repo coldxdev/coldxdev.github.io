f24b1d14911b758feea168a14c3f51f63b134e5c
coldxdev <62851883+coldxdev@users.noreply.github.com>
2020-10-24 18:58:37 +0300

    finally vikings

--- Vikings/js/main.js
+++ Vikings/js/main.js
@@ -1,8 +1,41 @@
-$(function () {
+$(function ()
+{
+
+
+$('.menu__btn').on('click', function(){
+  $('.menu__list').toggleClass('menu__list--active')
+});
+
+
+
   $("[data-fancybox]").fancybox({
     youtube: {
       showinfo: 0,
       VideoPlaybackQuality: hd,
     },
   });
+});
+
+$(".heroes__slider-img").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: ".heroes__slider-text",
+  prevArrow: '<button class="slick-btn slick-prev"><img src="./images/slider-btn.png" alt=""></button>',
+  nextArrow: '<button class="slick-btn slick-next"><img src="./images/slider-btn.png" style="transform: rotate(180deg);" alt=""></button>',
+  responsive: [
+    {
+    breakpoint: 740,
+      settings: {
+        arrows: 0,
+      }
+    }],
+});
+
+$(".heroes__slider-text").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: ".heroes__slider-img",
+  fade: true,
+  arrows: 0,
+  
 });
